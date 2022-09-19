7e8b73c7bd518a732768562a84e3bd61bf269d44
Martin Norén <martin.noren@sigma.se>
2022-09-19 15:11:04 +0200

    Add project files.

---
+++ IMS.UseCases/Inventories/ViewInventoriesByNameUseCase.cs
@@ -0,0 +1,20 @@
+﻿using IMS.CoreBusiness;
+using IMS.UseCases.PluginInterfaces;
+
+namespace IMS.UseCases.Inventories
+{
+    public class ViewInventoriesByNameUseCase
+    {
+        private readonly IInventoryRepository _inventoryRepository;
+
+        public ViewInventoriesByNameUseCase(IInventoryRepository inventoryRepository)
+        {
+            _inventoryRepository = inventoryRepository;
+        }
+
+        public async Task<IEnumerable<Inventory>> ExecuteAsync(string name = "")
+        {
+            return await _inventoryRepository.GetInventoriesByNameAsync(name);
+        }
+    }
+}
