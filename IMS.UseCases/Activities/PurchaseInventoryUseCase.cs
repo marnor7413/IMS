717f5b6f8b60fe02d49529e36d48ab04105c32c8
Martin Norén <martin.noren@sigma.se>
2022-09-22 15:06:21 +0200

    Feature: Purchase new inventory

---
+++ IMS.UseCases/Activities/PurchaseInventoryUseCase.cs
@@ -0,0 +1,30 @@
+﻿using IMS.CoreBusiness;
+using IMS.UseCases.Activities.Interfaces;
+using IMS.UseCases.PluginInterfaces;
+
+namespace IMS.UseCases.Activities
+{
+    public class PurchaseInventoryUseCase : IPurchaseInventoryUseCase
+    {
+        private readonly IInventoryTransactionRepository _inventoryTransactionRepository;
+        private readonly IInventoryRepository _inventoryRepository;
+
+        public PurchaseInventoryUseCase(IInventoryTransactionRepository inventoryTransactionRepository,
+            IInventoryRepository inventoryRepository)
+        {
+            _inventoryTransactionRepository = inventoryTransactionRepository;
+            _inventoryRepository = inventoryRepository;
+        }
+
+        public async Task ExecuteAsync(string poNumber,
+            Inventory inventory,
+            int quantity,
+            string doneBy)
+        {
+            _inventoryTransactionRepository.PurchaseAsync(poNumber, inventory, quantity, doneBy, inventory.Price);
+            inventory.Quantity += quantity;
+            await _inventoryRepository.UpdateInventoryAsync(inventory);
+
+        }
+    }
+}
--- IMS.Webapp/Program.cs
+++ IMS.Webapp/Program.cs
@@ -1,4 +1,6 @@
 using IMS.Plugins.InMemory;
+using IMS.UseCases.Activities;
+using IMS.UseCases.Activities.Interfaces;
 using IMS.UseCases.Inventories;
 using IMS.UseCases.Inventories.Interfaces;
 using IMS.UseCases.PluginInterfaces;
@@ -15,15 +17,19 @@ builder.Services.AddSingleton<WeatherForecastService>();
 
 builder.Services.AddSingleton<IInventoryRepository, InventoryRepository>();
 builder.Services.AddSingleton<IProductRepository, ProductRepository>();
+builder.Services.AddSingleton<IInventoryTransactionRepository, InventoryTransactionRepository>();
 
 builder.Services.AddTransient<IViewInventoriesByNameUseCase, ViewInventoriesByNameUseCase>();
 builder.Services.AddTransient<IAddInventoryUseCase, AddInventoryUseCase>();
 builder.Services.AddTransient<IEditInventoryUseCase, EditInventoryUseCase>();
 builder.Services.AddTransient<IViewInventoryByIdUseCase, ViewInventoryByIdUseCase>();
+
 builder.Services.AddTransient<IViewProductsByNameUseCase, ViewProductsByNameUseCase>();
 builder.Services.AddTransient<IAddProductUseCase, AddProductUseCase>();
 builder.Services.AddTransient<IViewProductByIdUseCase, ViewProductByIdUseCase>();
-builder.Services.AddTransient<IEditProductUseCase, EditProductUseCase>();
+builder.Services.AddTransient<IEditProductUseCase, EditProductUseCase>()
+    ;
+builder.Services.AddTransient<IPurchaseInventoryUseCase, PurchaseInventoryUseCase>();
 
 var app = builder.Build();
 
