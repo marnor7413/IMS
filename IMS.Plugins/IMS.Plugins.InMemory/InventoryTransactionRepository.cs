8e28ace5453e2b47c0bb51fd8b2a6c915b950ecf
Martin Norén <martin.noren@sigma.se>
2022-09-22 15:05:53 +0200

    Chore: create repo för inventory purchase transactions

---
+++ IMS.Plugins/IMS.Plugins.InMemory/InventoryTransactionRepository.cs
@@ -0,0 +1,24 @@
+﻿using IMS.CoreBusiness;
+using IMS.UseCases.PluginInterfaces;
+
+namespace IMS.Plugins.InMemory
+{
+    public class InventoryTransactionRepository : IInventoryTransactionRepository
+    {
+        public List<InventoryTransaction> InventoryTransactions = new();
+        public void PurchaseAsync(string poNumber, Inventory inventory, int quantity, string doneBy, double price)
+        {
+            InventoryTransactions.Add(new InventoryTransaction
+            {
+                PONumber = poNumber,
+                InventoryId = inventory.InventoryId,
+                QuantityBefore = inventory.Quantity,
+                ActivityType = InventoryTransactionType.PurchaseInventory,
+                QuantityAfter = inventory.Quantity + quantity,
+                TransactionDate = DateTime.Now,
+                DoneBy = doneBy,
+                UnitPrice = price
+            });
+        }
+    }
+}
