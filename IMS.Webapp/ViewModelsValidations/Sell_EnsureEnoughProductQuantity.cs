858266de2ebc04f162a6dedd9771d4cd8c7f2300
Martin Norén <martin.noren@sigma.se>
2022-09-26 14:12:04 +0200

    Feature: prohibit to sell more products than inventory allows

---
+++ IMS.Webapp/ViewModelsValidations/Sell_EnsureEnoughProductQuantity.cs
@@ -0,0 +1,24 @@
+﻿using IMS.Webapp.ViewModels;
+using System.ComponentModel.DataAnnotations;
+
+namespace IMS.Webapp.ViewModelsValidations
+{
+    public class Sell_EnsureEnoughProductQuantity : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            var sellViewModel = validationContext.ObjectInstance as SellViewModel;
+
+            if (sellViewModel.Product is not null)
+            {
+                if (sellViewModel.Product.Quantity < sellViewModel.QuantityToSell)
+                {
+                    return new ValidationResult($"There isn't enough product inventory. There is only {sellViewModel.Product.Quantity} in the warehouse",
+                        new [] { validationContext.MemberName ?? string.Empty });
+                };
+            };
+
+            return ValidationResult.Success;
+        }
+    }
+}
