62f229bf69921562fefb05a6fd935e10208abf38
Martin Norén <martin.noren@sigma.se>
2022-09-27 12:25:47 +0200

    Chore: configure dbcontext

--- IMS.CoreBusiness/Inventory.cs
+++ IMS.CoreBusiness/Inventory.cs
@@ -12,5 +12,7 @@ namespace IMS.CoreBusiness
         public int Quantity { get; set; }
         [Range(0, int.MaxValue, ErrorMessage = "Price must be greater or equal to 0")]
         public double Price { get; set; }
+
+        public List<ProductInventory> ProductInventories { get; set; } = new();
     }
 }
