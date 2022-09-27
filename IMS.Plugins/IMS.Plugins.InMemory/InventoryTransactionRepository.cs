f77d217646447ab934c591ed70feda6f137194ec
Martin Norén <martin.noren@sigma.se>
2022-09-27 14:26:50 +0200

    Bug: fix asyncronous calls in In-memory db

--- IMS.Plugins/IMS.Plugins.InMemory/InventoryTransactionRepository.cs
+++ IMS.Plugins/IMS.Plugins.InMemory/InventoryTransactionRepository.cs
@@ -43,7 +43,7 @@ namespace IMS.Plugins.InMemory
             return query;
         }
 
-        public void ProduceAsync(string productionNumber, Inventory inventory, int quantityToConsume, string doneBy, double price)
+        public Task ProduceAsync(string productionNumber, Inventory inventory, int quantityToConsume, string doneBy, double price)
         {
             InventoryTransactions.Add(new InventoryTransaction
             {
@@ -56,9 +56,12 @@ namespace IMS.Plugins.InMemory
                 DoneBy = doneBy,
                 UnitPrice = price
             });
+
+            return Task.CompletedTask;
+
         }
 
-        public void PurchaseAsync(string poNumber, Inventory inventory, int quantity, string doneBy, double price)
+        public Task PurchaseAsync(string poNumber, Inventory inventory, int quantity, string doneBy, double price)
         {
             InventoryTransactions.Add(new InventoryTransaction
             {
@@ -71,6 +74,9 @@ namespace IMS.Plugins.InMemory
                 DoneBy = doneBy,
                 UnitPrice = price
             });
+
+            return Task.CompletedTask;
+
         }
     }
 }
